33ad2c918151c2b9dbce900bf872838b850af74f
Leo Trojer <04troleo@rgtfo-me.it>
2021-02-04 09:08:42 +0100

    1.-Upload

---
+++ Ausgabe_1/Ausgabe_1.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, INPUT);
+}
+int i=0;
+bool vorher=0;
+bool nachher;
+void loop() {
+  Statusv = digitalRead(10);
+  if
+   
+                     
+}
